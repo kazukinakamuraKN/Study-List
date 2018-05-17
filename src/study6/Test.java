22ce246e6a7aa142bd7e9b3c8d2567ecfdce2d5e
kazuki.nakamura <kajirikajiri@gmail.com>
2018-05-17 22:36:55 +0900

    add mesoddo

--- src/study6/Test.java
+++ src/study6/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args){
 		hello(10,"a");
 		hello();
+		System.out.println(testString());
 	}
 
 	public static void hello(Integer a,String b){
@@ -11,6 +12,20 @@ public class Test {
 	}
 
 	public static void hello(){
-		System.out.println("hello");
+		System.out.println("...");
+	}
+
+	public static int testInt(){
+		return 10;
+	}
+
+	public static String testString(){
+		return "aiuel";
+	}
+
+	public static boolean testIntReturn(int a){
+		boolean result;
+		result = (a % 3 == 0);
+		return result;
 	}
 }
