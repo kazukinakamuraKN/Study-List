45f926cd07a9ab42b0121940ea7d2a7c1b25e6e7
kazuki.nakamura <kajirikajiri@gmail.com>
2018-05-17 21:03:17 +0900

    add practice1

--- src/study5/Array.java
+++ src/study5/Array.java
@@ -1,5 +1,11 @@
 package study5;
 
 public class Array {
-
+	public static void main (String[] args){
+		int a[] = {1,2,3,4,5,6,7,8,9,10};
+		int b[] = new int[10];
+		for(int c = 0; c < a.length; c++){
+			System.out.println(c * a.length);
+		}
+	}
 }
